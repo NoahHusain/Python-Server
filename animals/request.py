414d14ba1e910496365437e78ae54546454c8a73
Noah Husain <noahhusain@gmail.com>
2021-07-13 13:18:51 -0500

    finished chapter 4. Users can now add and delete animals.

--- animals/request.py
+++ animals/request.py
@@ -5,6 +5,7 @@ ANIMALS = [
         "species": "dog",
         "customerId": 1,
         "locationId": 2
+        "status": "Admitted"
     },
     {
         "id": 2,
@@ -12,6 +13,7 @@ ANIMALS = [
         "species": "dog",
         "customerId": 2,
         "locationId": 1
+        "status": "Admitted"
     },
     {
         "id": 3,
@@ -19,6 +21,7 @@ ANIMALS = [
         "species": "dog",
         "customerId": 3,
         "locationId": 2
+        "status": "Admitted"
     },
     {
         "id": 4,
@@ -26,6 +29,7 @@ ANIMALS = [
         "species": "bird",
         "customerId": 4,
         "locationId": 1
+        "status": "Admitted"
     },
     {
         "id": 5,
@@ -33,6 +37,7 @@ ANIMALS = [
         "species": "pig",
         "customerId": 5,
         "locationId": 2
+        "status": "Admitted"
     },
     {
         "id": 6,
@@ -40,6 +45,7 @@ ANIMALS = [
         "species": "cat",
         "customerId": 2,
         "locationId": 1
+        "status": "Admitted"
     }
 ]
 
@@ -63,3 +69,37 @@ def get_single_animal(id):
             requested_animal = animal
 
     return requested_animal
+
+
+def create_animal(animal):
+    # Get the id value of the last animal in the list
+    max_id = ANIMALS[-1]["id"]
+
+    # Add 1 to whatever that number is
+    new_id = max_id + 1
+
+    # Add an `id` property to the animal dictionary
+    animal["id"] = new_id
+
+    # Add the animal dictionary to the list
+    ANIMALS.append(animal)
+
+    # Return the dictionary with `id` property added
+    return animal
+
+
+def delete_animal(id):
+    # Initial -1 value for animal index, in case one isn't found
+    animal_index = -1
+
+    # Iterate the ANIMALS list, but use enumerate() so that you
+    # can access the index value of each item
+    for index, animal in enumerate(ANIMALS):
+        if animal["id"] == id:
+            # Found the animal. Store the current index.
+            animal_index = index
+
+    # If the animal was found, use pop(int) to remove it from list
+    if animal_index >= 0:
+        ANIMALS.pop(animal_index)
+
